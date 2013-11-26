7986fb3503eb11e4011b00eeba50663c8ca1896e
Pedro Angelo <pedro.fonini@smt.ufrj.br>
2013-11-26 12:53:05 -0200

    Update loadpng.h

--- loadpng.h
+++ loadpng.h
@@ -6,6 +6,7 @@ http://en.wikibooks.org/wiki/OpenGL_Programming/Intermediate/Textures#A_simple_l
 #define LOADPNG_H
 
 #include <GL/gl.h>
+#include <string>
 
 class loadpng_except : public exception {
     virtual const char* what() const throw() {
@@ -13,18 +14,17 @@ class loadpng_except : public exception {
     }
 } cant_loadpng;
 
-/** loadTexture
- *     loads a png file into an opengl texture object, using cstdio , libpng, and opengl.
+/** loadpng
+ *     loads a png file into an opengl texture object
  *
  *     \param filename : the png file to be loaded
  *     \param width : width of png, to be updated as a side effect of this function
  *     \param height : height of png, to be updated as a side effect of this function
  *
- *     \return GLuint : an opengl texture id.  Will be 0 if there is a major error,
- *                                     should be validated by the client of this function.
+ *     \return GLuint : an opengl texture id
  *
  */
 
-GLuint loadTexture(const string filename, int &width, int &height);
+GLuint loadpng(const string filename, int &width, int &height);
 
 #endif
