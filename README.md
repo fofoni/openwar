2d01f018068e9d2296c875a75f1f01ffd920594f
Pedro Angelo <pedro.fonini@smt.ufrj.br>
2013-11-27 03:58:20 -0200

    white sphere (?)

--- README.md
+++ README.md
@@ -3,3 +3,8 @@ OpenWAR
 
 WAR-like game based on OpenGL for the EEL882 (Introduction to Computer
 Graphics) chair at UFRJ (2013.2): http://www.lcg.ufrj.br/Cursos/CGECI.
+
+Earth texture taken/adapted from:
+http://www.gamedev.net/topic/606182-how-to-draw-a-earth/.
+
+TODO: earth.png shouldnt be NPOT
--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <cmath>
 
 #include "loadpng.h"
 
@@ -9,33 +10,46 @@
 # include <GL/glut.h>
 #endif
 
+const int TAU = 6.283185307179586477; // tau is 2*pi
+const int WORLD_LAT_QTD = 3;
+const int WORLD_LONG_QTD = 6;
+
+const int WORLD_LAT_EPS = TAU/(2*WORLD_LAT_QTD);
+const int WORLD_LONG_EPS = TAU/WORLD_LONG_QTD;
+
 namespace Key {
     const int ESC = 27;
 }
 
+float angle = 0;
+
+GLuint world_texture;
+int wt_width, wt_height;
+
 using namespace std;
 
-class texture {
-public:
-    GLuint id;
-    int w, h;
-    texture(int id=-1, int w=-1, int h=-1) : id(id), w(w), h(h) {}
-};
+// class texture {
+// public:
+//     GLuint id;
+//     int w, h;
+//     texture(int id=-1, int w=-1, int h=-1) : id(id), w(w), h(h) {}
+// };
 
-float angle = 0;
-float blue = .65;
-texture tex;
+// float blue = .65;
+// texture tex;
 
-class esc_except : public exception {
-    virtual const char* what() const throw() {
-        return "ESC key pressed";
-    }
-} escape;
+// class esc_except : public exception {
+//     virtual const char* what() const throw() {
+//         return "ESC key pressed";
+//     }
+// } escape;
 
 void handle_keypress(unsigned char key, int x, int y) {
+    cout  << int(key) <<endl;
     switch (key) {
         case Key::ESC:
-            throw escape;
+//             throw escape;
+            ;
     }
 }
 
@@ -47,10 +61,11 @@ void init_render() {
     glEnable(GL_LIGHTING);
     glEnable(GL_LIGHT0);
     glEnable(GL_NORMALIZE);
-    glEnable(GL_CULL_FACE);
+//     glEnable(GL_CULL_FACE);
     glShadeModel(GL_SMOOTH);
 
-    tex.id = loadpng("tex_teste.png", tex.w, tex.h);
+    world_texture = loadpng("earth.png", wt_width, wt_height);
+
 }
 
 void handle_resize(int w, int h) {
@@ -60,7 +75,7 @@ void handle_resize(int w, int h) {
     gluPerspective(
         45,                    // camera angle
         double(w) / double(h), // width-to-height ratio
-        1, 200                 // near and far z clipping coordinates
+        .5, 200                 // near and far z clipping coordinates
     );
 }
 
@@ -70,98 +85,121 @@ void draw_scene() {
 
     glMatrixMode(GL_MODELVIEW);
     glLoadIdentity();
-    glTranslatef(0, 0, -5);
 
     // ambient light
-    GLfloat ambient_color[] = {.4, .4, .4, 1};
+    GLfloat ambient_color[] = {.5, .5, .5, 1};
     glLightModelfv(GL_LIGHT_MODEL_AMBIENT, ambient_color);
 
     // positioned light
-    GLfloat light_color0[] = {1, .5, .5, 1};
-    GLfloat light_pos0[] = {0, 1, 0, 1};
+    GLfloat light_color0[] = {.5, .5, .5, 1};
+    GLfloat light_pos0[] = {-1, 1, 0, 1};
     glLightfv(GL_LIGHT0, GL_DIFFUSE, light_color0);
     glLightfv(GL_LIGHT0, GL_POSITION, light_pos0);
 
-    // Trapezoid
-    glPushMatrix();
-    glTranslatef(0, -1, 0);
-    glRotatef(angle, 0, 0, 1);
-    glEnable(GL_TEXTURE_2D); glBindTexture(GL_TEXTURE_2D, tex.id);
+    glTranslatef(0, 0, -4);
+
+    // make sphere representing earth
+    /* glEnable(GL_TEXTURE_2D); glBindTexture(GL_TEXTURE_2D, tex.id);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-    glBegin(GL_QUADS);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR); */
+    for (int j = 0; j < WORLD_LAT_QTD; j++) {
+        glBegin(GL_TRIANGLE_STRIP);
+        for (int i = 0; i <= WORLD_LONG_QTD; i++) {
+            glVertex3f(
+                sin(j*WORLD_LAT_EPS)*sin(i*WORLD_LONG_EPS),
+                cos(j*WORLD_LAT_EPS),
+                sin(j*WORLD_LAT_EPS)*cos(i*WORLD_LONG_EPS)
+            );
+            glVertex3f(
+                sin((j+1)*WORLD_LAT_EPS)*sin(i*WORLD_LONG_EPS),
+                cos((j+1)*WORLD_LAT_EPS),
+                sin((j+1)*WORLD_LAT_EPS)*cos(i*WORLD_LONG_EPS)
+            );
+        }
+        glEnd();
+    }
+    /* glDisable(GL_TEXTURE_2D); */
 
-        glColor3f(0.5f, 1, 1-blue);
-        glTexCoord2f(0, 0);
-        glVertex3f(-0.7f, -0.5f, 0.0f);
-//         glColor3f(0.0f, 0.9f, 0.0f);
-        glTexCoord2f(1, 0);
-        glVertex3f(0.7f, -0.5f, 0.0f);
-//         glColor3f(1.0f, 0.0f, 0.0f);
-        glTexCoord2f(1, 1);
-        glVertex3f(0.4f, 0.5f, 0.0f);
-        glColor3f(0.0f, 0.65, blue);
-        glTexCoord2f(0, 1);
-        glVertex3f(-0.4f, 0.5f, 0.0f);
-
-    glEnd();
-    glDisable(GL_TEXTURE_2D);
-    glPopMatrix();
-
-    // Pentagon
-    glPushMatrix();
-    glTranslatef(1, 1, 0);
-    glRotatef(angle, 1, 2, 3);
-    glScalef(.5, .5, .5);
-    glBegin(GL_TRIANGLES);
-
-        glVertex3f(-0.5f, -0.5f, 0.0f);
-        glVertex3f(0.5f, -0.5f, 0.0f);
-        glVertex3f(-0.5f, 0.0f, 0.0f);
-
-        glVertex3f(-0.5f, 0.0f, 0.0f);
-        glVertex3f(0.5f, -0.5f, 0.0f);
-        glVertex3f(0.5f, 0.0f, 0.0f);
-
-        glVertex3f(-0.5f, 0.0f, 0.0f);
-        glVertex3f(0.5f, 0.0f, 0.0f);
-        glVertex3f(0.0f, 0.5f, 0.0f);
-
-    glEnd();
-    glPopMatrix();
-
-    // Triangle
-    glPushMatrix();
-    glTranslatef(-1, 1, 0);
-    glRotatef(angle, 0, 1, 0);
-    glBegin(GL_TRIANGLES);
-
-        glNormal3f(1.f, 0.f, 1.f);
-        glVertex3f(0.5f, -0.5f, 0.0f);
-        glNormal3f(0.f, 1.f, 1.f);
-        glVertex3f(0.0f, 0.5f, 0.0f);
-        glNormal3f(-1.0f, 0.0f, 1.0f);
-        glVertex3f(-0.5f, -0.5f, 0.0f);
-
-    glEnd();
-    glPopMatrix();
+//     // Trapezoid
+//     glPushMatrix();
+//     glTranslatef(0, -1, 0);
+//     glRotatef(angle, 0, 0, 1);
+//     glEnable(GL_TEXTURE_2D); glBindTexture(GL_TEXTURE_2D, tex.id);
+//     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+//     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+//     glBegin(GL_QUADS);
+//
+//         glColor3f(0.5f, 1, 1-blue);
+//         glTexCoord2f(0, 0);
+//         glVertex3f(-0.7f, -0.5f, 0.0f);
+// //         glColor3f(0.0f, 0.9f, 0.0f);
+//         glTexCoord2f(1, 0);
+//         glVertex3f(0.7f, -0.5f, 0.0f);
+// //         glColor3f(1.0f, 0.0f, 0.0f);
+//         glTexCoord2f(1, 1);
+//         glVertex3f(0.4f, 0.5f, 0.0f);
+//         glColor3f(0.0f, 0.65, blue);
+//         glTexCoord2f(0, 1);
+//         glVertex3f(-0.4f, 0.5f, 0.0f);
+//
+//     glEnd();
+//     glDisable(GL_TEXTURE_2D);
+//     glPopMatrix();
+//
+//     // Pentagon
+//     glPushMatrix();
+//     glTranslatef(1, 1, 0);
+//     glRotatef(angle, 1, 2, 3);
+//     glScalef(.5, .5, .5);
+//     glBegin(GL_TRIANGLES);
+//
+//         glVertex3f(-0.5f, -0.5f, 0.0f);
+//         glVertex3f(0.5f, -0.5f, 0.0f);
+//         glVertex3f(-0.5f, 0.0f, 0.0f);
+//
+//         glVertex3f(-0.5f, 0.0f, 0.0f);
+//         glVertex3f(0.5f, -0.5f, 0.0f);
+//         glVertex3f(0.5f, 0.0f, 0.0f);
+//
+//         glVertex3f(-0.5f, 0.0f, 0.0f);
+//         glVertex3f(0.5f, 0.0f, 0.0f);
+//         glVertex3f(0.0f, 0.5f, 0.0f);
+//
+//     glEnd();
+//     glPopMatrix();
+//
+//     // Triangle
+//     glPushMatrix();
+//     glTranslatef(-1, 1, 0);
+//     glRotatef(angle, 0, 1, 0);
+//     glBegin(GL_TRIANGLES);
+//
+//         glNormal3f(1.f, 0.f, 1.f);
+//         glVertex3f(0.5f, -0.5f, 0.0f);
+//         glNormal3f(0.f, 1.f, 1.f);
+//         glVertex3f(0.0f, 0.5f, 0.0f);
+//         glNormal3f(-1.0f, 0.0f, 1.0f);
+//         glVertex3f(-0.5f, -0.5f, 0.0f);
+//
+//     glEnd();
+//     glPopMatrix();
 
     glutSwapBuffers();
 
 }
 
-void update(int value) {
-
-    angle += 2;
-    if (angle >= 180) angle -= 360;
-
-    blue += .0005;
-    if (blue >= 1) blue -= 1;
-
-    glutPostRedisplay();
-    glutTimerFunc(25, update, 0);
-
-}
+// void update(int value) {
+//
+//     angle += 2;
+//     if (angle >= 180) angle -= 360;
+//
+//     blue += .0005;
+//     if (blue >= 1) blue -= 1;
+//
+//     glutPostRedisplay();
+//     glutTimerFunc(25, update, 0);
+//
+// }
 
 int main(int argc, char** argv) {
 
@@ -175,12 +213,12 @@ int main(int argc, char** argv) {
     glutDisplayFunc(draw_scene);
     glutKeyboardFunc(handle_keypress);
     glutReshapeFunc(handle_resize);
-    glutTimerFunc(25, update, 0);
+//     glutTimerFunc(25, update, 0);
 
     try {
         glutMainLoop();
     }
-    catch (esc_except& e) {
+    catch (exception& e) {
         cout << "Bye." << endl;
     }
 
